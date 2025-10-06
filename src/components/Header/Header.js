4650f4da4d1fe74550efb853ac85ef95079e4c04
ralphdaryn <ralphdarync@gmail.com>
2025-10-05 21:37:01 -0400

    update portfolio name to gallery

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
             </li>
             <li className="header__item">
               <a className="header__link" href="/gallery">
-                Portfolio
+                Gallery
               </a>
             </li>
             <li className="header__item">
