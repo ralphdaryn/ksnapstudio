f03a9b5b63f40dcaabcc2d67d5ad85faae544208
ralphdaryn <ralphdarync@gmail.com>
2025-10-15 13:04:10 -0400

    style review page layout for desktop

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,3 @@
-// =====================
-// src/pages/Main/Main.js
-// =====================
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Main.scss";
